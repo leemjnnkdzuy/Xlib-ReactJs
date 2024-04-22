7eb845e8facc0bc827d2c8bf14f27e9d38e31dcb
LeeMjnnkDzuy <duylelv17@gmail.com>
2024-04-22 21:20:10 +0700

    fix component header

---
+++ src/pages/Search/index.js
@@ -0,0 +1,13 @@
+function Search() {
+    return (
+        <div>
+            <h1>Search</h1>
+            <p>
+                This is a simple web application that demonstrates how to build a web application using vanilla
+                JavaScript.
+            </p>
+        </div>
+    );
+}
+
+export default Search;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,7 @@
+//import HeaderOnly
+import { HeaderOnly } from '~/components/Layout/';
+
+//import Pages
 import Welcome from '~/pages/Welcome';
 import Err from '~/pages/Err';
 import About from '~/pages/About';
@@ -6,6 +10,8 @@ import Login from '~/pages/Login';
 import Signup from '~/pages/Signup';
 import ResetPassword from '~/pages/ResetPassword';
 import Home from '~/pages/Home';
+import Search from '~/pages/Search';
+
 
 const publicRoutes = [
     {
@@ -39,6 +45,11 @@ const publicRoutes = [
     {
         path: '/home',
         component: Home,
+        layout: HeaderOnly,
+    },
+    {
+        path: '/search',
+        component: Search,
         layout: null,
     },
 ];
